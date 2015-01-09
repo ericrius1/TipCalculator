94501fd144a756660575f997eeeb705fb06d309c
ericrius1 <ericrius1@gmail.com>
2015-01-08 22:35:52 -0800

    added line animations which respond to differing tip amounts. Added currency thousand separators

--- tips/MainViewController.swift
+++ tips/MainViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billBackgroundView: UIView!
+    @IBOutlet weak var topBarLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topBarRightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomBarLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomBarRightConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
  
@@ -21,6 +26,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        
+        
+        self.title = "Tip Calculator"
+        
+        self.billBackgroundView.layer.cornerRadius = 10.0
+        self.billBackgroundView.layer.zPosition = -1
         
     }
 
@@ -46,8 +57,37 @@ class ViewController: UIViewController {
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+
+        
+        var formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        // formatter.locale = NSLocale.currentLocale() // This is the default
+        tipLabel.text = formatter.stringFromNumber(tip)
+        totalLabel.text = formatter.stringFromNumber(total)
+        
+        
+//        tipLabel.text = String(format: "$%.2f", tip)
+//        totalLabel.text = String(format: "$%.2f", total)
+        
+
+        var newConstraintValue = CGFloat(0)
+        
+        
+        if(tipControl.selectedSegmentIndex == 0) {
+            newConstraintValue = CGFloat(50)
+        } else if(tipControl.selectedSegmentIndex == 1) {
+            newConstraintValue = CGFloat(30)
+        } else {
+            newConstraintValue = CGFloat(10)
+        }
+        
+        UIView.animateWithDuration(0.7) {
+            self.topBarRightConstraint.constant = newConstraintValue
+            self.topBarLeftConstraint.constant = newConstraintValue
+            self.bottomBarRightConstraint.constant = newConstraintValue * 2
+            self.bottomBarLeftConstraint.constant = newConstraintValue * 2
+            self.view.layoutIfNeeded()
+        }
 
         
     }
